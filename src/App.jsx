76090bc9303be12d7ef0c840aca0a889168c159e
HmSani6699 <shahinur04042002@gmail.com>
2023-04-04 01:35:49 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl bg-lime-500">Welcome to my TailwindCssWith vite world</h1>
       <Navbar></Navbar>
+      <h1 className="text-5xl bg-lime-500">Welcome to my TailwindCssWith vite world</h1>
+      
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { Bars4Icon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
-    const routes = [
+    cont [open ,setOpen ]=useState(false);
+   
+      const routes = [
         {
+          name: 'Home',
           id: 1,
-          name: 'Route A',
-          time: '10:00 AM'
+          path: '/'
         },
         {
+          name: 'About',
           id: 2,
-          name: 'Route B',
-          time: '11:30 AM'
+          path: '/about'
         },
         {
+          name: 'Contact',
           id: 3,
-          name: 'Route C',
-          time: '1:00 PM'
+          path: '/contact'
         },
         {
-          id: 4,
-          name: 'Route D',
-          time: '3:00 PM'
-        },
-        {
-          id: 5,
-          name: 'Route E',
-          time: '5:00 PM'
-        }
+            name: 'Dashboard',
+            id: 4,
+            path: '/dashboard'
+          },
+          {
+            name: 'Profile',
+            id: 5,
+            path: '/profile'
+          }
       ];
+            
       
     return (
         <nav>
-            <ul>
+            <Bars4Icon className="h-6 w-6 text-purple-500" />
+            <ul className='md:flex text-2xl'>
             {
-                routes.map(route=><li key={route.id}>{route.name}</li>)
+                routes.map(route=><Link key={route.id} route={route}></Link>)
             }
             </ul>
         </nav>
