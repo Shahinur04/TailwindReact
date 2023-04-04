a0696ca887e0cea8a6a4e03b5cf28cdcbc721119
HmSani6699 <shahinur04042002@gmail.com>
2023-04-05 01:08:48 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import PriceList from "./Components/PriceList/PriceList";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <h1 className="text-5xl text-blue-500 mt-8">Welcome to my TailwindCssWith vite world</h1>
-      
+      <h1 className="text-6xl text-blue-500 mt-8 p-5 ">Welcome to my TailwindCssWith vite world</h1>
+      <PriceList></PriceList>
     </div>
   );
 }
---
+++ src/Components/PriceList/PriceList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PriceList = () => {
+    return (
+        <div>
+            <h2 className='text-4xl bg-emerald-300'>Kew tomake vison vlobasuk tumi onno kro hole lukiye anmone vison kaduk</h2>
+        </div>
+    );
+};
+
+export default PriceList;
