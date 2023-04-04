9350ea84df070a762bd74425d4d7440900ef569c
HmSani6699 <shahinur04042002@gmail.com>
2023-04-05 02:15:50 +0600

    features

--- src/Components/PriceCart/PriceCart.jsx
+++ src/Components/PriceCart/PriceCart.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const PriceCart = ({price}) => {
-    return (
-        <div>
-            <h2 >{price.option}</h2>
-            <h5 >
-                <span className='text-xl font-bold text-purple-700'>{price.price}</span>/Mon</h5>
-        </div>
-    );
+const PriceCart = ({ price }) => {
+  return (
+    <div className="bg-indigo-700 mt-4 rounded-md text-center p-5 my-3">
+      <h2>{price.option}</h2>
+      <h5>
+        <span className="text-xl font-bold text-purple-700">{price.price}</span>
+        /Mon
+      </h5>
+      <h2>Features:{}</h2>
+      {
+        price.features.map(feature=><li>{feature}</li>)
+      }
+    </div>
+  );
 };
 
 export default PriceCart;
