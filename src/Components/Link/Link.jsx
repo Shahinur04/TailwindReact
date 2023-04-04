cb86424c58b6d81f00e5039903678d42befdba32
HmSani6699 <shahinur04042002@gmail.com>
2023-04-05 00:50:41 +0600

    update navebar

--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = ({route}) => {
     return (
-        <li className='mr-12'>
+        <li className='mr-12 hover:bg-lime-600'>
            <a href={route.path}>{route.name}</a>
         </li>
     );
